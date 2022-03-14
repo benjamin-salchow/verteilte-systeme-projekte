114cf625e31fa034c5c00e91ee12afd119f169d6
Benjamin Salchow <benjamin.salchow@mbr-net.de>
2022-03-14 17:27:09 +0100

    Add nodemon example

---
+++ node-client-server-extended-with-database-nodemon/README.md
@@ -0,0 +1,113 @@
+# Client - (Web-)Server Extended mit einer Datenbank in Node.js mit nodemon
+
+## Allgemein
+
+Dies ist dasselbe Beispiel wie `node-client-server-extendet-with-database`, nur dass in diesem Fall noch nodemon verwendet wird. Dieses Tool ermöglicht für die Entwicklung ein Live-Update und automatischen Neustart des Node.js Servers für eine schnellere Entwicklung.
+
+Bitte schaue zuerst das Beispiel `node-client-server-extended-with-database` an: [- node-client-server-extended-with-database Beispiel ->](../node-client-server-extended-with-database/README.md)
+
+In dieser Anleitung wird nur auf alle Erweiterungen des Extended-Beispiels der Datenbank eingegangen.
+
+![Screenshot](screenshot.png)
+
+### Nodemon
+
+Damit das Live-Update automatisch funktioniert, muss zuerst in dem `server`-Verzeichnis `npm install` ausgeführt werden.
+
+Das Verzeichnis `server` wird in den Container direkt übergeben. Somit wird das lokale Verzeichnis im Container durch den sogenannten `mount` zur Verfügung gestellt.
+Dies bedeutet, dass das Löschen, Anlegen oder Ändern von Dateien bzw. Verzeichnissen im Server auch im Host durchgeführt werden.
+
+`nodemon` erkennt jegliche Änderung an den Dateien und startet den Server automatisch neu. Danach muss jedoch die Seite im Browser neu geladen werden.
+
+
+**Wichtig:** `nodemon` ist ein Development Tool und das sollte für die Abgabe entfernt werden.
+
+1. Verzeichnismount: <todo>
+2. Löschen des Überschreibens durch command <todo>
+3. Entfernen der nodemon dependency
+
+
+### Static Files
+
+Da das Verzeichnis vom Host eingebunden wird, werden Änderungen an den `Static Files` automatisch angewendet und können durch eine einfache Aktualisierung im Browser direkt angezeigt werden (`F5`).
+
+
+## Installation und Ausführung
+
+**Hinweis:** Beachte, dass die `docker-compose.yml` im Hauptverzeichnis des Projektes liegt. Die Ordnerstruktur enthält einen Ordner `client` und einen Ordner `server`, welche jeweils die benötigten Dateien sowie eine eigene `Dockerfile` haben, welche die Container beschreiben. Die Container sind unabhängig und unterscheiden sich. Innerhalb der `docker-compose.yml` wird unter `build` der Ordner angegeben, welcher die `Dockerfile` beinhaltet.
+
+In den Verzeichnisen `server` müssen vor dem ersten Start mittels:
+```sh
+npm install
+```
+alle definierten Bibliotheken im `package.js` heruntergeladen werden. Sonst kann der Container nicht gestartet werden.
+
+**Hinweis:** nodemon muss nicht lokal installiert werden. Es reicht vollkommen aus, dass dies nur im Container läuft. Dort wird es in Zeile <todo> installiert.
+
+Danach kann in diesem Ordner folgender Befehl ausgeführt werden:
+
+```sh
+# start mit
+docker-compose up --build
+```
+
+**Hinweis:** Sollte es Probleme geben, muss unter Umständen das `:Z` in der `docker-compose.yaml` entfernt werden (<todo>).
+
+Danach starten alle Container wie gewohnt. Es kann jedoch sein, dass der Server mehrfach abstürzt, da dieser auf die Verfügbarkeit der Datenbank warten muss.
+
+Wenn jetzt eine Änderung auf dem Computer durchgeführt wird, wird diese durch den `Volume-mount` automatisch an den Container weitergegeben und man sieht folgenden Log:
+
+```sh
+server_1        | [nodemon] restarting due to changes...
+server_1        | [nodemon] starting `node server.js`
+```
+
+Dabei wird auch im Log ausgegeben, wenn die `database.sql` geladen wird: `meinecooledb_1  | 2020-04-10 13:38:59+00:00 [Note] [Entrypoint]: /usr/local/bin/docker-entrypoint.sh: running /docker-entrypoint-initdb.d/database.sql`.
+
+Um alle Container zu stoppen, können diese mittels [strg] + [c] beendet werden.
+
+**Hinweis:** Sollte der Container nicht herunterfahren, dann kann [strg] + [c] nochmals gedrückt werden, um dies zu beschleunigen.
+
+Der Output sieht wie folgt aus:
+```sh
+Gracefully stopping... (press Ctrl+C again to force)
+Stopping node-client-server-extended-with-database_server_1       ...
+Stopping node-client-server-extended-with-database_phpmyadmin_1   ...
+Stopping node-client-server-extended-with-database_meinecooledb_1 ...
+# wenn nochmal [strg] + [c] gedrückt wird:
+Stopping node-client-server-extended-with-database_server_1       ... done
+Stopping node-client-server-extended-with-database_phpmyadmin_1   ... done
+Stopping node-client-server-extended-with-database_meinecooledb_1 ... done
+```
+
+**Wichtig:** Der Parameter `--build` sorgt dafür, dass bei jedem Aufruf von `docker-compose up` alle Container neu gebaut werden, damit alle Änderungen im `server.js` sowie im Datenbankschema `database.sql` in den jeweiligen Container integriert werden.
+
+
+## Informationen
+
+ * Nodemon:
+   * Homepage: https://nodemon.io/
+   * Dokumentation: https://github.com/remy/nodemon#nodemon
+ * Express
+   * NPM: https://www.npmjs.com/package/express
+   * Dokumentation: https://expressjs.com/en/4x/api.html
+   * Routing: https://expressjs.com/en/guide/routing.html
+   * Getting Started: https://expressjs.com/en/starter/installing.html
+ * MariaDB
+   * Dockerhub: https://hub.docker.com/_/mariadb/
+   * Homepage: https://mariadb.org/
+   * Dokumentation: https://mariadb.org/documentation/
+ * MySQL-Client
+   * NPM: https://www.npmjs.com/package/mysql
+   * Repository: https://github.com/mysqljs/mysql
+ * Bootstrap (CSS für Table, usw.)
+   * Homepage: https://getbootstrap.com/
+   * Alerts: https://getbootstrap.com/docs/4.3/components/alerts/
+   * Tabelle: https://getbootstrap.com/docs/4.0/content/tables/
+   * Buttons: https://getbootstrap.com/docs/4.3/components/buttons/
+ * W3Schools:
+   * Button onclick Event: https://www.w3schools.com/jsref/event_onclick.asp
+   * JSON HTML Table: https://www.w3schools.com/js/js_json_html.asp
+   * AJAX: https://www.w3schools.com/xml/ajax_intro.asp
+   * Node.js mysql: https://www.w3schools.com/nodejs/nodejs_mysql_where.asp
+   * JSON: https://www.w3schools.com/js/js_json.asp
---
+++ node-client-server-extended-with-database-nodemon/server/server.js
@@ -0,0 +1,215 @@
+'use strict';
+
+const express = require('express');
+
+// Database
+const mysql = require('mysql');
+// Database connection info - used from environment variables
+var dbInfo = {
+    connectionLimit : 10,
+    host: process.env.MYSQL_HOSTNAME,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
+};
+
+var connection = mysql.createPool(dbInfo);
+console.log("Conecting to database...");
+// connection.connect(); <- connect not required in connection pool
+
+// SQL Database init.
+// In this current demo, this is done by the "database.sql" file which is stored in the "db"-container (./db/).
+// Alternative you could use the mariadb basic sample and do the following steps here:
+/*
+connection.query("CREATE TABLE IF NOT EXISTS table1 (task_id INT AUTO_INCREMENT PRIMARY KEY, title VARCHAR(255) NOT NULL, description TEXT, created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)  ENGINE=INNODB;", function (error, results, fields) {
+    if (error) throw error;
+    console.log('Answer: ', results);
+});
+*/
+// See readme.md for more information about that.
+
+// Check the connection
+connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+    if (error) throw error; // <- this will throw the error and exit normally
+    // check the solution - should be 2
+    if (results[0].solution == 2) {
+        // everything is fine with the database
+        console.log("Database connected and works");
+    } else {
+        // connection is not fine - please check
+        console.error("There is something wrong with your database connection! Please check");
+        process.exit(5); // <- exit application with error code e.g. 5
+    }
+});
+
+
+// Constants
+const PORT = process.env.PORT || 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+
+// Features for JSON Body
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Entrypoint - call it with: http://localhost:8080/ -> redirect you to http://localhost:8080/static
+app.get('/', (req, res) => {
+    console.log("Got a request and redirect it to the static page");
+    // redirect will send the client to another path / route. In this case to the static route.
+    res.redirect('/static');
+});
+
+// Another GET Path - call it with: http://localhost:8080/special_path
+app.get('/special_path', (req, res) => {
+    res.send('This is another path');
+});
+
+// Another GET Path that shows the actual Request (req) Headers - call it with: http://localhost:8080/request_info
+app.get('/request_info', (req, res) => {
+    console.log("Request content:", req)
+    res.send('This is all I got from the request:' + JSON.stringify(req.headers));
+});
+
+// POST Path - call it with: POST http://localhost:8080/client_post
+app.post('/client_post', (req, res) => {
+    if (typeof req.body !== "undefined" && typeof req.body.post_content !== "undefined") {
+        var post_content = req.body.post_content;
+        console.log("Client send 'post_content' with content:", post_content)
+        // Set HTTP Status -> 200 is okay -> and send message
+        res.status(200).json({ message: 'I got your message: ' + post_content });
+    }
+    else {
+        // There is no body and post_contend
+        console.error("Client send no 'post_content'")
+        // Set HTTP Status -> 400 is client error -> and send message
+        res.status(400).json({ message: 'This function requries a body with "post_content"' });
+    }
+});
+
+// ###################### BUTTON EXAMPLE ######################
+// POST path for Button 1
+app.post('/button1_name', (req, res) => {
+    // Load the name from the formular. This is the ID of the input:
+    const name = req.body.name
+    // Print it out in console:
+    console.log("Client send the following name: " + name + " | Button1")
+    // Send JSON message back - this could be also HTML instead.
+    res.status(200).json({ message: 'I got your message - Name is: ' + name });
+    // More information here: https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs/forms
+})
+
+// GET path for Button 2
+app.get('/button2', (req, res) => {
+    // This will generate a random number and send it back:
+    const random_number = Math.random();
+    // Print it out in console:
+    console.log("Send the following random number to the client: " + random_number + " | Button2")
+    // Send it to the client / webbrowser:
+    res.send("Antwort: g" + random_number);
+    // Instead of plain TXT - the answer could be a JSON
+    // More information here: https://www.w3schools.com/xml/ajax_intro.asp
+});
+// ###################### BUTTON EXAMPLE END ######################
+
+
+// ###################### DATABASE PART ######################
+// GET path for database
+app.get('/database', (req, res) => {
+    console.log("Request to load all entries from table1");
+    // Prepare the get query
+    connection.query("SELECT * FROM `table1`;", function (error, results, fields) {
+        if (error) {
+            // we got an errror - inform the client
+            console.error(error); // <- log error in server
+            res.status(500).json(error); // <- send to client
+        } else {
+            // we got no error - send it to the client
+            console.log('Success answer from DB: ', results); // <- log results in console
+            // INFO: Here could be some code to modify the result
+            res.status(200).json(results); // <- send it to client
+        }
+    });
+});
+
+// DELETE path for database
+app.delete('/database/:id', (req, res) => {
+    // This path will delete an entry. For example the path would look like DELETE '/database/5' -> This will delete number 5
+    let id = req.params.id; // <- load the ID from the path
+    console.log("Request to delete Item: " + id); // <- log for debugging
+
+    // Actual executing the query to delete it from the server
+    // Please keep in mind to secure this for SQL injection!
+    connection.query("DELETE FROM `table1` WHERE `table1`.`task_id` = " + id + ";", function (error, results, fields) {
+        if (error) {
+            // we got an errror - inform the client
+            console.error(error); // <- log error in server
+            res.status(500).json(error); // <- send to client
+        } else {
+            // Everything is fine with the query
+            console.log('Success answer: ', results); // <- log results in console
+            // INFO: Here can be some checks of modification of the result
+            res.status(200).json(results); // <- send it to client
+        }
+    });
+});
+
+// POST path for database
+app.post('/database', (req, res) => {
+    // This will add a new row. So we're getting a JSON from the webbrowser which needs to be checked for correctness and later
+    // it will be added to the database with a query.
+    if (typeof req.body !== "undefined" && typeof req.body.title !== "undefined" && typeof req.body.description !== "undefined") {
+        // The content looks good, so move on
+        // Get the content to local variables:
+        var title = req.body.title;
+        var description = req.body.description;
+        console.log("Client send database insert request with 'title': " + title + " ; description: " + description); // <- log to server
+        // Actual executing the query. Please keep in mind that this is for learning and education.
+        // In real production environment, this has to be secure for SQL injection!
+        connection.query("INSERT INTO `table1` (`task_id`, `title`, `description`, `created_at`) VALUES (NULL, '" + title + "', '" + description + "', current_timestamp());", function (error, results, fields) {
+            if (error) {
+                // we got an errror - inform the client
+                console.error(error); // <- log error in server
+                res.status(500).json(error); // <- send to client
+            } else {
+                // Everything is fine with the query
+                console.log('Success answer: ', results); // <- log results in console
+                // INFO: Here can be some checks of modification of the result
+                res.status(200).json(results); // <- send it to client
+            }
+        });
+    }
+    else {
+        // There is nobody with a title nor description
+        console.error("Client send no correct data!")
+        // Set HTTP Status -> 400 is client error -> and send message
+        res.status(400).json({ message: 'This function requries a body with "title" and "description' });
+    }
+});
+// ###################### DATABASE PART END ######################
+
+
+
+
+// All requests to /static/... will be redirected to static files in the folder "public"
+// call it with: http://localhost:8080/static
+app.use('/static', express.static('public'))
+
+// Start the actual server
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
+
+// Start database connection
+const sleep = (milliseconds) => {
+    return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
+
+
+
+
+
+
+
+
+
